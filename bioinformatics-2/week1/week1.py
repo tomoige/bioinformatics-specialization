d117947a6336d91b94f1544cffc3ae917782786a
Thomas Cormican <thomascormican123@gmail.com>
2026-06-22 21:21:09 +0100

    update

--- bioinformatics-2/week1/week1.py
+++ bioinformatics-2/week1/week1.py
@@ -8,11 +8,27 @@ text = "TATGGGGTGC"
 k = 3
 print(kmer_compositions(text, k))
 
-with open("week1-1.txt") as f:
-    lines = f.read().strip().splitlines()
-    k = int(lines[0])
-    text = lines[1]
-    res = kmer_compositions(text, k)
-    print(" ".join(res))
-    with open("week1-1-output.txt", "x") as f2:
-        f2.write(" ".join(res))
+# with open("week1-1.txt") as f:
+#     lines = f.read().strip().splitlines()
+#     k = int(lines[0])
+#     text = lines[1]
+#     res = kmer_compositions(text, k)
+#     print(" ".join(res))
+#     with open("week1-1-output.txt", "x") as f2:
+#         f2.write(" ".join(res))
+
+def genome_path(genome):
+    string = ""
+    for i, genomeslice in enumerate(genome):
+        if(i == len(genome) - 1):
+            string += genomeslice
+        else:
+            string += genomeslice[0]
+    return string
+
+genome = ["ACCGA", "CCGAA", "CGAAG", "GAAGC", "AAGCT"]
+print(genome_path(genome))
+
+with open("week1-2.txt") as f:
+    genome = f.read().strip().split(" ")
+    print(genome_path(genome))
