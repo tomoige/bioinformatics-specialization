f3b66fa7ea021adbe58bea90e7df7dbdfc844eea
Thomas Cormican <thomascormican123@gmail.com>
2026-07-10 17:27:51 +0100

    update

--- bioinformatics-2/week2/week2.py
+++ bioinformatics-2/week2/week2.py
@@ -13,8 +13,6 @@ adjacency_list = {
     9: [6],
 }
 
-
-
 def random_walk(adjacency_list, start_node, current_node, first, accum):
     accum.append(current_node)
     options = adjacency_list[current_node]
@@ -25,7 +23,7 @@ def random_walk(adjacency_list, start_node, current_node, first, accum):
     adjacency_list[current_node].pop(rand_index)
     if not first and (start_node == current_node):
         print("got to the last place")
-        return (accum, adjacency_list)
+        return accum, adjacency_list
     else:
         return random_walk(adjacency_list, start_node, next_node, False, accum)
 
@@ -34,9 +32,54 @@ def find_unexplored_in_cycle(adjacency_list, cycle):
         if(len(adjacency_list[position]) > 0):
             return position
 
-(cycle, adj_list) = random_walk(adjacency_list, 0, 0, True, [])
-print(cycle, adj_list)
-new_start = find_unexplored_in_cycle(adj_list, cycle) # gets a node that has unexplored edges that is in the cycle
-(cycle2, adj_list_2) = random_walk(adj_list, new_start, new_start, True, [])
-print(cycle2, adj_list_2)
+def update_full_path(current_path, new_start, new_cycle):
+    index = current_path.index(new_start)
+    firstHalf = current_path[0:index]
+    secondHalf = current_path[index+1:]
+    firstHalf.extend(new_cycle)
+    firstHalf.extend(secondHalf)
+    
+    return firstHalf
+
+def check_adj_list_empty(adj_list):
+    for (key, value) in adj_list.items():
+        if value:
+            return False
+    return True
+
+def get_eulerian_cycle(adjacency_list, start_node):
+    empty = check_adj_list_empty(adjacency_list)
+    eulerian_cycle = []
+
+    # initial cycle
+    if(not empty):
+        eulerian_cycle, adjacency_list = random_walk(adjacency_list, start_node, start_node, True, [])
+    empty = check_adj_list_empty(adjacency_list)
+
+    while(not empty):
+        
+        # get next cycle and update adjacency_list
+        new_start = find_unexplored_in_cycle(adjacency_list, eulerian_cycle)
+        (cycle, adjacency_list) = random_walk(adjacency_list, new_start, new_start, True, [])
+        print("updating: ", eulerian_cycle, " with: ", cycle)
+        eulerian_cycle = update_full_path(eulerian_cycle, new_start, cycle)
+        empty = check_adj_list_empty(adjacency_list)
+
+    return eulerian_cycle
+
+print(get_eulerian_cycle(adjacency_list, 2))
+
+
+
+
+# (cycle, adj_list) = random_walk(adjacency_list, 0, 0, True, [])
+# print(cycle, adj_list)
+# print("list status: ", check_adj_list_empty(adj_list))
+# new_start = find_unexplored_in_cycle(adj_list, cycle) # gets a node that has unexplored edges that is in the cycle
+# (cycle2, adj_list_2) = random_walk(adj_list, new_start, new_start, True, [])
+# print(cycle2, adj_list_2)
+# print("list status: ", check_adj_list_empty(adj_list))
+
+# print(update_full_path(cycle, new_start, cycle2))
+
 
