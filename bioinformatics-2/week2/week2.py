8480aaff98e2d75e1d5db7c6db3c89a0e2f96f67
Thomas Cormican <thomascormican123@gmail.com>
2026-07-13 17:06:17 +0100

    update

--- bioinformatics-2/week2/week2.py
+++ bioinformatics-2/week2/week2.py
@@ -66,21 +66,69 @@ def get_eulerian_cycle(adjacency_list, start_node):
 
 print(get_eulerian_cycle(adjacency_list, 6))
 
-with open("week2-1.txt") as f:
+# with open("week2-1.txt") as f:
+#     lines = f.read().strip().splitlines()
+#     adjacency_list = {}
+#     for line in lines:
+#         splitlines = line.split(":")
+#         key = int(splitlines[0])
+#         value = list(map(int, splitlines[1][1:].split(" ")))
+#         adjacency_list[key] = value
+#     print("answer: ")
+#     res = get_eulerian_cycle(adjacency_list, 1)
+#     res_string = ""
+#     for item in res:
+#         res_string += str(item) + " "
+#     print(res_string)
+#     with open("week2-1-answer.txt", "x") as f2:
+#         f2.write(res_string)
+
+def find_end(adjacency_list):
+    nodes_with_next = []
+    for (key, value) in adjacency_list.items():
+        nodes_with_next.append(key)
+    for i in adjacency_list:
+        for j in adjacency_list[i]:
+            if j not in nodes_with_next:
+                return j
+
+sample_list = {
+    0: [2],
+    1: [3],
+    2: [1],
+    3: [0, 4],
+    6: [3, 7],
+    7: [8],
+    8: [9],
+    9: [6]
+}
+
+print(find_end(sample_list))
+
+def work_back(current, adjacency_list, accum):
+    accum.insert(0, current)
+    empty = check_adj_list_empty(adjacency_list)
+    if empty:
+        return accum
+    for key, value in adjacency_list.items():
+        for node in value:
+            if node == current:
+                new_adj = adjacency_list
+                new_adj[key].remove(current)
+                return work_back(key, new_adj, accum)
+
+print("returning: ", work_back(4, sample_list, []))
+
+with open("week2-2.txt") as f:
     lines = f.read().strip().splitlines()
-    adjacency_list = {}
+    adj_list = {}
     for line in lines:
-        splitlines = line.split(":")
-        key = int(splitlines[0])
-        value = list(map(int, splitlines[1][1:].split(" ")))
-        adjacency_list[key] = value
-    print("answer: ")
-    res = get_eulerian_cycle(adjacency_list, 1)
-    res_string = ""
-    for item in res:
-        res_string += str(item) + " "
-    print(res_string)
-    with open("week2-1-answer.txt", "x") as f2:
-        f2.write(res_string)
+        split_line = line.split(": ")
+        adj_list[int(split_line[0])] = list(map(int, split_line[1].split(" ")))
+    print(adj_list)
+    end = find_end(adj_list)
+    print(end)
+    res = work_back(end, adj_list, [])
+    print(res)
 
-
+# def get_eulerian_path(adjacency_list)
